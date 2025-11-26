8bda9d83fc78ecf0000e2782c3c9d17874ce9564
mhmharshani <mhmharshani@gmail.com>
2025-11-26 16:38:24 +0530

    Add cards for recipes and API call used in JS

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,1 +1,18 @@
 console.log("Js Loaded!");
+
+let title = document.getElementById("recipe_title");
+let thumb_img = document.getElementById("thumb_img");
+
+
+async function api_call(){
+    await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data.meals[0].strMeal)
+        title.innerText = data.meals[0].strMeal;
+        thumb_img.src = data.meals[0].strMealThumb;
+    })
+}
+
+api_call();
